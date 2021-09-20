bb874775456ef776d4fd411fda2095913306bac0
Mikael Rusin <mikael.rusin@gmail.com>
2021-09-20 11:53:38 +0200

    Register Page Base
    Just saving the Register Page Base

---
+++ frontend/src/views/RegisterPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mainDiv">
+    <div class="navbar">
+      <div class="locationBox Box">Location<div class="locationBar"><input type="text" placeholder="Location" class="searchBar"></div></div>
+      <div class="guestBox Box">Guests<div class="guestsBar"><input type="number" min=1 placeholder="Guests" class="guestsBar"></div></div>
+      <div class="startDateBox Box">Start Date<div class="startDateBar"><input min="2021-09-17" type="date"></div></div>
+      <div class="endDateBox Box">End Date<div class="endDateBar"><input type="date" min="2021-09-17" max="2021-10-17"></div></div>
+      <div class="priceBox Box">Price<div class="priceBar"><input placeholder=0 type="number" min=0></div></div>
+      <button class="searchButton" @click="search" type="button">Search</button>
+      <router-link class="Login" to="/">Login</router-link>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+    };
+  },
+  async mounted() {
+
+  },
+  methods: {
+    search(){
+    }
+  },
+};
+</script>
+<style scoped>
+
+</style>
