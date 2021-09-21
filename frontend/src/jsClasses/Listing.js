509018598c366e2a05b0a9cd6ed9b119449f038c
Mikael Rusin <mikael.rusin@gmail.com>
2021-09-21 16:17:59 +0200

    Integrated the Base to be able to Display Search Results
    Integrated the base in terms of the item to be rendered

---
+++ frontend/src/jsClasses/Listing.js
@@ -0,0 +1,76 @@
+export default class Listing {
+  constructor(title, description, image_url, location, number_guests, price, listing_start_date, listing_end_date) {
+    this.title = title;
+    this.description = description;
+    this.image_url = image_url;
+    this.location = location;
+    this.number_guests = number_guests;
+    this.price = price;
+    this.listing_start_date = listing_start_date;
+    this.listing_end_date = listing_end_date;
+  }
+  
+  retrieveTheTitleOfTheListing() {
+    return this.title;
+  }
+
+  setTheTitleOfTheListing(newTitleOfThisListing) {
+    this.title = newTitleOfThisListing;
+  }
+
+  retrieveTheDescriptionOfTheListing() {
+    return this.description;
+  }
+
+  setTheDescriptionOfTheListing(newDescription) {
+    this.description = newDescription;
+  }
+
+  retrieveTheLinkToThePicture() {
+    return this.image_url;
+  }
+
+  setTheLinkToThePicture(newImageURL) {
+    this.image_url = newImageURL;
+  }
+
+  retrieveTheLocation() {
+    return this.location;
+  }
+
+  setTheLocation(newLocation) {
+    this.location = newLocation;
+  }
+
+  retrieveTheAmountOfGuests() {
+    return this.number_guests;
+  }
+
+  setTheAmountOfGuests(newAmountOfGuests) {
+    this.number_guests = newAmountOfGuests;
+  }
+
+  setThePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  getThePrice() {
+    return this.price;
+  }
+
+  setTheStartDateOfThisListing(newStartDate) {
+    this.listing_start_date = newStartDate;
+  }  
+
+  getTheStartDateOfThisListing() {
+    return this.listing_start_date;
+  }
+
+  setTheEndDateOfThisListing(newEndDate) {
+    this.listing_end_date;
+  }
+
+  getTheEndDateOfThisListing() {
+    return this.listing_end_date;
+  }
+}
