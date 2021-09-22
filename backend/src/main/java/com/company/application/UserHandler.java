a17202c0983f74eea9abcce191373915f6f7264a
Mikael Rusin <mikael.rusin@gmail.com>
2021-09-22 23:21:23 +0200

    Integrated REST Path Interactions through Backend
    Integrated so that Users can register into the Database,
    along with being able to process a Response.
    
    In light of this, i had to enable CORS interactions, allowing
    of credentials and manually interacting with the header
    elements to some extent.
    
    Further more, i had to integrate a solution to parsing a
    Readable Stream - Which was resolved by .then() functional
    parsings.
    
    A user can now register, and the Username they register
    with in the Database is Unique.

---
+++ backend/src/main/java/com/company/application/UserHandler.java
@@ -0,0 +1,42 @@
+package com.company.application;
+
+import com.company.infrastructure.UserRepository;
+import express.Express;
+import com.company.domain.User;
+public class UserHandler {
+
+    private final Express app;
+    private final UserRepository theUserRepository;
+
+    public UserHandler(Express app, UserRepository theUserRepository){
+        this.app = app;
+        this.theUserRepository = theUserRepository;
+        initUserHandler();
+    }
+
+
+    private void initUserHandler() {
+        app.post("/user", (req, res) -> {
+            User newUser = req.body(User.class);
+            res.append("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.append("Access-Control-Allow-Credentials", "true");
+            try{
+                User seeIfTheUserExists = theUserRepository.findByUsername(newUser.getUsername());
+                if(seeIfTheUserExists.getClass().equals(User.class)){
+
+                    res.json("That username is already taken.");
+                    return;
+                }
+            }
+            catch(Exception e){
+                if(e.getMessage() == "No entity found for query"){
+                    theUserRepository.save(newUser);
+                    res.json("Made a new user!");
+                }
+                else{
+                    res.json(e);
+                }
+            }
+        });
+    }
+}
