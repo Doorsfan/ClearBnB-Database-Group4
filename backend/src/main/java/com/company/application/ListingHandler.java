777a9dc959ed0a33413e9bf5c3ffff0e52499363
Mikael Rusin <mikael.rusin@gmail.com>
2021-09-23 00:08:57 +0200

    Made base for the listingHandler

---
+++ backend/src/main/java/com/company/application/ListingHandler.java
@@ -0,0 +1,34 @@
+package com.company.application;
+
+import com.company.domain.Listing;
+import com.company.domain.User;
+import com.company.infrastructure.ListingRepository;
+import express.Express;
+
+public class ListingHandler {
+
+    private final Express app;
+    private final ListingRepository theListingRepository;
+
+    public ListingHandler(Express app, ListingRepository theListingRepository){
+        this.app = app;
+        this.theListingRepository = theListingRepository;
+        initListingHandler();
+    }
+
+
+    private void initListingHandler() {
+        app.post("/listing", (req, res) -> {
+            Listing newListing = req.body(Listing.class);
+            res.append("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.append("Access-Control-Allow-Credentials", "true");
+            try{
+                theListingRepository.save(newListing);
+                res.json("Posted a new listing!");
+            }
+            catch(Exception e){
+                res.json(e);
+            }
+        });
+    }
+}
