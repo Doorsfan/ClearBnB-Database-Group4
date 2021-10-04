d28b2aed4eba30d50580a6fe144e38b52e908d05
dogbite1337 <kocak_cc25@hotmail.com>
2021-10-04 14:41:24 -0500

    Added TTL of 1 hour
    Added TTL of 1 hour for items in the cache. This is an arbitrary number, but allows for testing the cache.

--- backend/src/main/java/com/company/infrastructure/UserCacheRepository.java
+++ backend/src/main/java/com/company/infrastructure/UserCacheRepository.java
@@ -5,8 +5,11 @@ import com.google.gson.GsonBuilder;
 import redis.clients.jedis.Jedis;
 import com.google.gson.Gson;
 
+import java.util.concurrent.TimeUnit;
+
 public class UserCacheRepository {
-    private Jedis jedis; // the Redis connection (cache)
+    private final Jedis jedis; // the Redis connection (cache)
+    private static final int ttl = 3600; // "Time to Live" for cache (in seconds)
 
     public UserCacheRepository(Jedis jedis) {
         this.jedis = jedis;
@@ -29,6 +32,7 @@ public class UserCacheRepository {
         System.out.println("Adding user to cache");
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         jedis.set(path, gson.toJson(user));
+        jedis.expire(path, ttl);
     }
 
     public void remove(String path) {
